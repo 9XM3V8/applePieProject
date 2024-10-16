43eca7b54b8085d1b9337bb86af28de2b86de362
Dylan <dylanlouder@gmail.com>
2024-10-15 18:58:12 -0600

    Finished

---
+++ applePieTests/applePieTests.swift
@@ -0,0 +1,17 @@
+//
+//  applePieTests.swift
+//  applePieTests
+//
+//  Created by Dylan on 10/15/24.
+//
+
+import Testing
+@testable import applePie
+
+struct applePieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ applePieUITests/applePieUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  applePieUITestsLaunchTests.swift
+//  applePieUITests
+//
+//  Created by Dylan on 10/15/24.
+//
+
+import XCTest
+
+final class applePieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
